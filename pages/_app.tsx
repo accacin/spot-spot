eb517b80a1f6ffa261197b5a03f8568a6ca019c5
Daniel Brownlow <Daniel Brownlow>
2022-10-13 20:25:09 +0100

    Add initial maps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { Page } from "../components/Layout";
+import { Page } from "../components/layout";
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
+  integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
+/>;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/map/create.tsx
+++ pages/map/create.tsx
@@ -1,16 +1,33 @@
-import type { NextPage } from "next";
+import Script from "next/script";
 import Head from "next/head";
+import dynamic from "next/dynamic";
+
+import type { NextPage } from "next";
 
 const CreateMap: NextPage = () => {
+  const MapWithNoSSR = dynamic(() => import("../../components/map/Map"), {
+    ssr: false
+  });
+
   return (
     <>
       <Head>
         <title>Creata a Map</title>
         <meta name="description" content="Create a Map" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
+          integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
+        />
       </Head>
       <section className="pt-20">
-        <h1>Hello</h1>
+        <Script
+          src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"
+          integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg="
+        ></Script>
+        <div id="map"></div>
+        <MapWithNoSSR />
       </section>
     </>
   );
