769145e90b7536b4bd04911f71e8c821ed6562b0
Daniel Brownlow <Daniel Brownlow>
2022-10-17 21:30:16 +0100

    Add new dropdown using headlessui library

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button/Button";
 export { default as LoginButton } from "./LoginButton/LoginButton";
-export { default as ProfilePicture } from "./ProfilePicture/ProfilePicture"
+export { default as ProfilePicture } from "./ProfilePicture/ProfilePicture";
+export { default as Dropdown } from "./Dropdown/Dropdown";
--- components/layout/Nav/Nav.tsx
+++ components/layout/Nav/Nav.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import LocationIcon from "../../../public/location-icon.svg";
-import { LoginButton } from "../../../components/common";
+import { Dropdown } from "../../../components/common";
 
 const Nav = () => {
   return (
@@ -14,7 +14,7 @@ const Nav = () => {
           </div>
         </Link>
         <div className="flex text-lg select-none">
-          <LoginButton />
+          <Dropdown />
         </div>
       </div>
     </nav>
