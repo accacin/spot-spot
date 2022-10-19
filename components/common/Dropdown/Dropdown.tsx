a07a171a6783dcb06e69b722fd0d7400a8ee5012
Daniel Brownlow <Daniel Brownlow>
2022-10-19 21:44:38 +0100

    Fix logged in dropdown style

--- components/common/Dropdown/Dropdown.tsx
+++ components/common/Dropdown/Dropdown.tsx
@@ -36,9 +36,9 @@ const Dropdown = ({ name, image }: PropTypes) => {
     <div className="w-56 text-right">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <Menu.Button className="inline-flex w-full items-center justify-center rounded-md bg-blue px-4 py-2 text-sm font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             <ProfilePicture url={image} />
-            {name}
+            <span className="ml-2">{name}</span>
             <ChevronDownIcon
               className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
               aria-hidden="true"
--- components/common/ProfilePicture/ProfilePicture.tsx
+++ components/common/ProfilePicture/ProfilePicture.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import Image from "next/future/image";
 interface Props {
   url?: string;
 }
 const ProfilePicture = ({ url }: Props) => {
   if (url == null) return <h1>no image</h1>;
-  return <Image src={url} alt="User Profile Picture" width="25" height="25" />;
+  return (
+    <Image
+      src={url}
+      alt="User Profile Picture"
+      width="35"
+      height="35"
+      className="rounded-full border-gray-300 border-2"
+    />
+  );
 };
 
 export default ProfilePicture;
