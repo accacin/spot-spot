801287a25ad9830d448c42250f7f632f0d63392a
Daniel Brownlow <Daniel Brownlow>
2022-11-01 21:54:26 +0000

    Implement basic form for adding a location list

--- pages/lists/index.tsx
+++ pages/lists/index.tsx
@@ -1,9 +1,20 @@
-import Script from "next/script";
 import Head from "next/head";
-
 import type { NextPage } from "next";
 
 const Lists: NextPage = () => {
+  const handleSubmit = async (event: any) => {
+    console.log(event);
+    const formData = new FormData(event.target);
+    const dataObject = Object.fromEntries(formData);
+
+    console.log(dataObject);
+
+    // await fetch("/api/form", {
+    //   method: "POST",
+    //   body: JSON.stringify(dataObject),
+    // });
+    event.target.reset();
+  };
   return (
     <>
       <Head>
@@ -11,13 +22,70 @@ const Lists: NextPage = () => {
         <meta name="description" content="Create a Map" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="flex w-full flex-col flex-grow-1 justify-center items-center">
-        <h1 className="mt-4 text-4xl font-normal leading-normal mt-0 mb-2">
-          My Lists
-        </h1>
-        <p>You have no lists? Create one here: </p>
-        <button>Create</button>
-      </section>
+      <div className="container mx-auto">
+        <section className="flex w-full mx-auto px-4 py-4 md:px-6 lg:px-8 ">
+          <h1 className="mt-4 text-4xl font-normal leading-normal mt-0 mb-2">
+            My Location Lists
+          </h1>
+        </section>
+        <section className="flex w-full flex-col mx-auto px-4 pb-4 md:px-6 lg:px-8 ">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Name
+              </label>
+              <input
+                type="listname"
+                id="listname"
+                name="listname"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="My Favourite Restaurants"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                rows={4}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Leave a comment..."
+              ></textarea>
+            </div>
+            <div className="flex items-start mb-6">
+              <div className="flex items-center h-5">
+                <input
+                  id="public"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                />
+              </div>
+              <label
+                htmlFor="remember"
+                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Public?
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+        </section>
+      </div>
     </>
   );
 };
